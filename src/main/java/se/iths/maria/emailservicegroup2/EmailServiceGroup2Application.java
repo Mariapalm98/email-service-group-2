459cf567e3a4da6dce194ef3bbce38eb5d9573ba
Mariapalm98 <mariapalma.mpp@gmail.com>
2026-05-19 11:25:34 +0200

    Initial commit

---
+++ src/main/java/se/iths/maria/emailservicegroup2/EmailServiceGroup2Application.java
@@ -0,0 +1,13 @@
+package se.iths.maria.emailservicegroup2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceGroup2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailServiceGroup2Application.class, args);
+	}
+
+}
