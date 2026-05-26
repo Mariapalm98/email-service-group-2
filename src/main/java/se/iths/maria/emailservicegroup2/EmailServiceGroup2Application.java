540f9aed50a9d0223fa543b012b41cdc172f56f7
Mariapalm98 <mariapalma.mpp@gmail.com>
2026-05-26 16:42:44 +0200

    tagit bort emailConfig klassen - lagt till properties för email och lösenord

--- src/main/java/se/iths/maria/emailservicegroup2/EmailServiceGroup2Application.java
+++ src/main/java/se/iths/maria/emailservicegroup2/EmailServiceGroup2Application.java
@@ -3,7 +3,10 @@ package se.iths.maria.emailservicegroup2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "se.iths.maria.emailservicegroup2",
+        "se.iths.maria.springmessengers"
+})
 public class EmailServiceGroup2Application {
 
     public static void main(String[] args) {
