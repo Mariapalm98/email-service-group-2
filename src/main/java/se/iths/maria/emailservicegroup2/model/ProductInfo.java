179212410d90f9fc631dbb95d2104704491cf08a
Mariapalm98 <mariapalma.mpp@gmail.com>
2026-05-29 15:29:00 +0200

    ändrat build email

--- src/main/java/se/iths/maria/emailservicegroup2/model/ProductInfo.java
+++ src/main/java/se/iths/maria/emailservicegroup2/model/ProductInfo.java
@@ -3,6 +3,7 @@ package se.iths.maria.emailservicegroup2.model;
 import java.math.BigDecimal;
 
 public record ProductInfo(
+        Long id,
         String name,
         String description,
         BigDecimal price,
